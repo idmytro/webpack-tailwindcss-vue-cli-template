ff3ecb71cc21cc9febc1496ddf01c2d66e319d3c
Dmytro Cisar <dmytro.cisar@gmail.com>
2018-09-05 01:25:00 +0100

    + postcss nested

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import')(),
+    require('postcss-nested')(),
     require('tailwindcss')('./build-utils/tailwindcss.config.js'),
     require('autoprefixer')(),
   ],
